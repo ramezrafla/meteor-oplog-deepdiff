b54fbca2d595c3791e5484bcf1fb8047e5c683bf
Ramez Rafla <ramez@zegenie.com>
2017-06-12 10:56:17 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var deepDiff = require('deep-diff').diff;
+
+MongoInternals.OplogObserveDriver.prototype._changePublished = function (id, oldDoc, newDoc) {
+  var self = this;
+  Meteor._noYieldsAllowed(function () {
+    self._published.set(id, self._sharedProjectionFn(newDoc));
+    var projectedNew = self._projectionFn(newDoc);
+    var projectedOld = self._projectionFn(oldDoc);
+    var diff = deepDiff(oldDoc,newDoc);
+    if (diff) {
+      var changed  = {}
+      _.each(diff,function(part) {
+        var field = part.path.join(".");
+        if (part.kind == 'D') changed[field] = null;
+        // note: if there is no part.item.rhs it means item was deleted
+        else if (part.kind == 'A') changed[field + '.' + part.index] = part.item.rhs;
+        else changed[field] = part.rhs;
+      });
+      console.log(changed);
+      if (!_.isEmpty(changed)) {
+        self._multiplexer.changed(id, changed);
+      }
+    }
+  });
+}
---
+++ package.js
@@ -0,0 +1,23 @@
+// XXX We should revisit how we factor MongoDB support into (1) the
+// server-side node.js driver [which you might use independently of
+// livedata, after all], (2) minimongo [ditto], and (3) Collection,
+// which is the class that glues the two of them to Livedata, but also
+// is generally the "public interface for newbies" to Mongo in the
+// Meteor universe. We want to allow the components to be used
+// independently, but we don't want to overwhelm the user with
+// minutiae.
+
+Package.describe({
+  name: 'ramez:oplog-deepdiff',
+  summary: "Deep diff for DDP",
+  version: '0.9.0'
+});
+
+Npm.depends({
+  "deep-diff": "0.3.8"
+});
+
+Package.onUse(function (api) {
+  api.use(['mongo@1.1.18','ecmascript'], 'server');
+  api.addFiles('index.js', 'server');
+});
